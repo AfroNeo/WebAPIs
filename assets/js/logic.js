479c24e12acd678fd7f46a9c675cd18f384f3584
Matthew <mbrooks9622@gmail.com>
2023-01-16 14:18:29 +0000

    implemented timer and set questions in to an array

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,97 @@
-function button2Func() {
-    alert("File 2 Connected")
+// function button2Func() {
+//     alert("File 2 Connected")
+// }
+
+const startQuizBtn = document.getElementById('start');
+const questionsBox = document.getElementById('questions');
+const questionTitle = document.getElementById('question-title');
+const questionChoices = document.getElementById('choices');
+const timeEl = document.getElementById('time');
+const endScreen = document.getElementById('end-screen');
+
+let currentIndex = 0
+let timer;
+let time = 60;
+
+function setTimer(){
+timer = setInterval(function(){
+time--
+timeEl.textContent = time;
+if (time == 0){ 
+    // endQuiz()
 }
+
+},1000)
+
+
+}
+
+
+function displayNextQuestion() {
+   if(currentIndex===questions.length){
+    endQuiz()
+   }
+
+    questionsBox.classList.remove('hide');
+    
+    console.log('click heard from displayNextQuestion');
+    // set the title and choices to the questionsBox
+    questionTitle.textContent = questions[currentIndex].question;
+    // now assign the values of the choices to questionChoices
+    for(let i = 0; i < questions[currentIndex].answers.length; i++) {
+        const btn = document.createElement('button');
+        btn.textContent = questions[currentIndex].answers[i];
+        questionChoices.append(btn);
+    }
+   
+}
+function checkAnswer(choiceAnswer){
+// console.log(choiceAnswer)
+if(choiceAnswer===questions[currentIndex].correctAnswer){
+currentIndex++
+// questionsBox.innerHTML=""
+questionChoices.innerHTML=""
+displayNextQuestion();
+}
+else{
+      
+    currentIndex++
+    time-=5;
+    // questionsBox.innerHTML=""
+    
+    questionChoices.innerHTML=""
+    displayNextQuestion();
+
+}
+
+}
+function endQuiz(){
+    clearInterval(timer)
+    questionsBox.style.display="none"
+    endScreen.classList.remove("hide")
+}
+//add local storage here
+
+
+// add event listeners to the very bottom of your page
+startQuizBtn.addEventListener('click', function() {
+    // hide the start button
+    this.classList.add('hide');
+    // run the displayNextQuestion function
+
+    displayNextQuestion();
+    setTimer()
+
+})
+
+questionChoices.addEventListener('click',()=>{
+    let userChoice = this.event.target.textContent
+    // console.log(userChoice)
+
+    checkAnswer(userChoice)
+
+
+})
+
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,32 @@
-function button1Func() {
-    alert("File 1 Connected")
-}
-console.log(button1Func)
+// function button1Func() {
+//     alert("File 1 Connected")
+// }
+// console.log(button1Func)
 
-let timer;
-let count
+// I'm accessing the log method of my console object. I pass the method an argument
+// in this case the argument is a string.
+console.log('questions.js file has been read first');
 
+// let timer;
+// let count
+
+const questions = [
+    {
+       question: 'Hello, how are you doing today? (test)', // I Just as well could have used double quotes
+       answers: ['Great!', 'So-so', 'Meh', 'Fantastic'],
+       correctAnswer: 'Fantastic'
+    },
+    {
+        question: 'What day is today? (test', // I Just as well could have used double quotes
+        answers: ['Great!', 'Sunday', "Meh', 'Fantastic"],
+        correctAnswer: 'Sunday'
+    },
+    {
+        question: 'What is the correct JavaScript syntax to change the content of the HTML element below? <p id="demo">This is a demonstration.</p>',
+        answers: ['document.getElement("p").innerHTML = "Hello World!"', '#document.innerHTML = "Hello World!"', 'document.getElementById("demo).innerHTML = "Hello World!"'],
+        correctAnswer: 'document.getElementById("demo).innerHTML = "Hello World!"'
+
+        // continue with my questions and possible answers
+    }
+
+];
