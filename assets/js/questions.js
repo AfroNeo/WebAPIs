218cbc724f70b7f19cde095357cf410a179a630a
Matthew <mbrooks9622@gmail.com>
2023-01-16 18:03:44 +0000

    quiz questions added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,23 +10,60 @@ console.log('questions.js file has been read first');
 // let timer;
 // let count
 
+
+//These are my Quiz Questions:
 const questions = [
     {
-       question: 'Hello, how are you doing today? (test)', // I Just as well could have used double quotes
-       answers: ['Great!', 'So-so', 'Meh', 'Fantastic'],
-       correctAnswer: 'Fantastic'
+       question: '1. How do you write "Hello World" in an alert box?',
+       answers: ['alert("Hello World");', 'msgBox("Hello World");', 'alertBox("Hello World");', 'msg("Hello World");'],
+       correctAnswer: 'alert("Hello World");'
     },
     {
-        question: 'What day is today? (test', // I Just as well could have used double quotes
-        answers: ['Great!', 'Sunday', "Meh', 'Fantastic"],
-        correctAnswer: 'Sunday'
+        question: '2. How do you create a function in JavaScript?',
+        answers: ['function = myFunction()', 'function myFunction()', "function:myFunction()"],
+        correctAnswer: 'function myFunction()'
     },
     {
-        question: 'What is the correct JavaScript syntax to change the content of the HTML element below? <p id="demo">This is a demonstration.</p>',
-        answers: ['document.getElement("p").innerHTML = "Hello World!"', '#document.innerHTML = "Hello World!"', 'document.getElementById("demo).innerHTML = "Hello World!"'],
-        correctAnswer: 'document.getElementById("demo).innerHTML = "Hello World!"'
+        question: '3. How do you write "Hello World" in an alert box?',
+        answers: ['alert("Hello World");', 'msgBox("Hello World");', 'alertBox("Hello World");','msg("Hello World");'],
+        correctAnswer: 'alert("Hello World");'
 
         // continue with my questions and possible answers
-    }
+    },
+    {
+        question: '4. How do you create a function in JavaScript?',
+        answers: ['function = myFunction()', 'function myFunction()', 'Function:myFunction()'],
+        correctAnswer: 'function myFunction()'
+     },
+     {
+        question: '5. How do you call a function named "myFunction"?',
+        answers: ['myFuntion', 'call myFunction', 'call function my Function()'],
+        correctAnswer: 'myFunction()'
+     },
+     {
+        question: '6. How to write an IF statement in JavaScript?',
+        answers: ['if (i == 5)', 'if i = 5 then', 'if i == 5 then', 'if i = 5'],
+        correctAnswer: 'if (i == 5)'
+     },
+     {
+        question: '7. How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+        answers: ['if (i!=5)', 'if i <> 5', 'if (i <> 5)', 'if i =! 5 then'],
+        correctAnswer: 'if (i!= 5)'
+     },
+     {
+        question: '8. How does a WHILE loop start?',
+        answers: ['while (i <= 10)', 'while (i <= 10; i++)', 'while i = 1 to 10'],
+        correctAnswer: 'while (i <= 10)'
+     },
+     {
+        question: '9. How does a FOR loop start?',
+        answers: ['for (i <= 5; i++)', 'for i = 1 to 5', 'for (i = 0; i <= 5; i++)', 'for (i = 0; i <= 5)'],
+        correctAnswer: 'for (i = 0; i <= 5; i++)'
+     },
+     {
+        question: '10. What is the correct way to write a JavaScript array?',
+        answers: ['var colors = ["red", "green", "blue"]', 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = (1:"red", 2:"green", 3:"blue")', 'var colors = "red", "green", "blue"'],
+        correctAnswer: 'var colors = ["red", "green", "blue"]'
+     },
 
 ];
